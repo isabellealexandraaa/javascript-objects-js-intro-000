ede6715164120868f185b999972dd626bb739919
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-04-14 21:57:43 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -9,8 +9,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 
 var playlist = {
-  Slowdive: 'Alison', 
-  My Bloody Valentine: 'Sometimes', 
+  Slowdive: 'Alison',
+  My Bloody Valentine: 'Sometimes',
   Phil Ochs: "Here's to the State of Mississippi";
 }
 
